5b946b346c6701294d01f92638323a9f5d5d2787
hburn7 <hburnett777@gmail.com>
2022-01-14 13:29:44 -0500

    Created basic module

---
+++ commands/basic.py
@@ -0,0 +1,16 @@
+from discord.commands import slash_command
+from discord.ext import commands
+
+
+class Basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(guild_ids=[931367517564317707])
+    async def ping(self, ctx):
+        """Checks Yuzuru's latency"""
+        await ctx.respond(f'🏓 Pong! {ctx.bot.latency * 1000:.2f}ms ')
+
+
+def setup(bot):
+    bot.add_cog(Basic(bot))
