678aef0e67661171ec3889dff514b76bba373b51
hburn7 <hburnett777@gmail.com>
2022-01-14 17:02:36 -0500

    NSFW age confirmation

--- database/models/db_models.py
+++ database/models/db_models.py
@@ -1,11 +1,15 @@
 from datetime import datetime
-from peewee import CharField, DateTimeField, IntegerField, BigIntegerField, TextField, ForeignKeyField, IdentityField
+from peewee import CharField, DateTimeField, IntegerField, \
+    BigIntegerField, TextField, ForeignKeyField, IdentityField, \
+    BooleanField
 from database import YuzuruModel
 
 
 class User(YuzuruModel):
     id = IdentityField(unique=True)
     command_count = IntegerField(default=0)
+    nsfw_age_confirm = BooleanField(default=False)
+    nsfw_age_confirm_timestamp = DateTimeField(null=True)
     timestamp = DateTimeField(default=datetime.utcnow())
     user_id = BigIntegerField()
 
