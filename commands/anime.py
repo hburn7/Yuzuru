7f9928306f2b1f67c5cd2652bfc4e4a97519659c
hburn7 <hburnett777@gmail.com>
2022-01-14 13:29:28 -0500

    Deleted test command

--- commands/anime.py
+++ commands/anime.py
@@ -6,11 +6,6 @@ class Anime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(guild_ids=[931367517564317707])
-    async def yuzuru(self, ctx):
-        """This is a test description"""
-        await ctx.respond("Hey!")
-
 
 def setup(bot):
     bot.add_cog(Anime(bot))
