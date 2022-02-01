b2255e61522f98c1cd26549517878587272c4181
hburn7 <hburnett777@gmail.com>
2022-02-01 10:16:25 -0500

    Update defaults & added section for data directories

--- core/config.py
+++ core/config.py
@@ -16,34 +16,49 @@ class Config:
     postgres_database: str
     docker: bool
 
+    neko_dir: str
+    lewd_dir: str
+    nsfw_dir: str
+
 
 def get_or_create_config():
     config_path = Path('./config.ini')
+
+    logger.debug(f'Searching for config file at {config_path.absolute()}...')
+
     config = ConfigParser()
 
-    key = "BOT_CONFIG"
+    bot_key = "BOT_CONFIG"
+    data_key = "DATA"
 
     if not config_path.exists():
-        logging.info('Config file not found, creating.')
+        logger.debug('Config file not found, creating...')
         # Create and return default
-        config[key] = {
-            "token": "<delete and replace with bot token>",
-            "postgres_username": "PostgresUsername",
-            "postgres_pass": "PostgresPassword",
+        config[bot_key] = {
+            "token": "YOUR_TOKEN_HERE",
+            "postgres_username": "postgres",
+            "postgres_pass": "password",
             "postgres_host": "localhost",
-            "postgres_port": 5432,
-            "postgres_database": "Yuzuru",
-            "docker": True
+            "postgres_port": 5500,
+            "postgres_database": "yuzuru",
+            "docker": False
         }
+        config[data_key] = {
+            "neko_dir": "",
+            "lewd_dir": "",
+            "nsfw_dir": ""
+        }
+
         with open(config_path.absolute(), 'w') as conf:
             config.write(conf)
-        logging.info('Successfully created new config file.')
-        config.read(config_path.absolute())
-        data = config[key]
-        return Config(data["token"])
-    else:
-        # Return existing
-        config.read(config_path.absolute())
-        data = config[key]
-        return Config(data["token"], data["postgres_username"], data["postgres_pass"],
-                      data["postgres_host"], int(data["postgres_port"]), data["postgres_database"], data["docker"] == "True")
+        logger.debug(f'Successfully created new config file at {config_path.absolute()}.')
+
+    # Return existing / newly created log
+    logger.debug(f'Configuration file found.')
+    config.read(config_path.absolute())
+    bot_data = config[bot_key]
+    data_data = config[data_key]
+    return Config(bot_data["token"], bot_data["postgres_username"], bot_data["postgres_pass"],
+                  bot_data["postgres_host"], int(bot_data["postgres_port"]), bot_data["postgres_database"],
+                  bot_data["docker"] == "True", neko_dir=data_data["neko_dir"], lewd_dir=data_data["lewd_dir"],
+                  nsfw_dir=data_data["nsfw_dir"])
