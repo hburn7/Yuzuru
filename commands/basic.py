a3fd50b61332d9cc7ad08cfe60685b9a02b93bc1
hburn7 <hburnett777@gmail.com>
2022-02-01 11:56:08 -0500

    removed guild id restriction

--- commands/basic.py
+++ commands/basic.py
@@ -6,7 +6,7 @@ class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(guild_ids=[931367517564317707])
+    @slash_command()
     async def ping(self, ctx):
         """Checks Yuzuru's latency to Discord servers"""
         await ctx.respond(f'🏓 Pong! {ctx.bot.latency * 1000:.2f}ms ')
